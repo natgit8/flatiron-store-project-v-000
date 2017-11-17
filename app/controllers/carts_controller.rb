3aee9bc9b95f40ffda8184a0ce4e6f9dcd26f9d7
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-11-16 23:09:25 -0500

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,17 @@
 class CartsController < ApplicationController
 
+  def index
+    @carts = Cart.all
+  end 
+
+  def show
+    @cart = Cart.find(params[:id])
+  end
+
+  def checkout
+    cart = Cart.find params[:id]
+    cart.checkout
+    redirect_to cart_path(cart)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'store#index', as: 'store'
 
-  resources :items, only: [:show, :index]
-  resources :categories, only: [:show, :index]
-  resources :users, only: [:show]
-  resources :carts
-  resources :line_items, only: [:create]
-  resources :orders, only: [:show]
+    resources :items, only: [:show, :index]
+    resources :categories, only: [:show, :index]
+    resources :users, only: [:show]
+    resources :carts
+    resources :line_items, only: [:create]
+    resources :orders, only: [:show]
 
-  post 'carts/:id/checkout', to: 'carts#checkout', as: 'checkout'
+    post 'carts/:id/checkout', to: 'carts#checkout', as: 'checkout'
 
+    get '/users/sign_out', to: 'store#index'
 end
